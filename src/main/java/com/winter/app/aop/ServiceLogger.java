126532d83c13eb698a44ee9f19064131ccef018d
nammuung <audrbs2368@naver.com>
2024-03-07 17:19:14 +0900

    00

---
+++ src/main/java/com/winter/app/aop/ServiceLogger.java
@@ -0,0 +1,35 @@
+package com.winter.app.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component  
+@Aspect
+@Slf4j
+public class ServiceLogger {
+
+	
+	
+	//공통로직 Advice
+	@Around("execution( * com.winter.app.**.*Service.*(..))")
+	public Object log(ProceedingJoinPoint joinPoint)throws Throwable{
+		
+		log.info("======== Service start =========");
+		Object obj = joinPoint.proceed();
+		
+		log.info("======매개변수 : {}", joinPoint.getArgs());	
+		
+		
+		log.info("======= 리턴값 : {}", obj);
+		
+		return obj;
+	}
+	
+	
+	
+	
+}

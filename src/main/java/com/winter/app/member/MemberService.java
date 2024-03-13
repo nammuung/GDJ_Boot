a4541bb7da0e32bd30ccbe6a3da8e1dc81551573
nammuung <audrbs2368@naver.com>
2024-03-13 10:42:03 +0900

    2

--- src/main/java/com/winter/app/member/MemberService.java
+++ src/main/java/com/winter/app/member/MemberService.java
@@ -18,65 +18,65 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberService implements UserDetailsService {
 	@Autowired
 	private MemberDAO memberDAO;
-	
+
 	@Autowired
-	//@Qualifier("ps")
+	// @Qualifier("ps")
 	private PasswordEncoder passwordEncoder;
-		
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		MemberVO memberVO = new MemberVO();
 		memberVO.setUsername(username);
-		
+
 		log.info("----- 로그인 진행 ----");
 		log.info("---- {} ---- ", username);
-		
+		System.out.println("test");
+
 		try {
 			memberVO = memberDAO.getDetail(memberVO);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return memberVO;
 	}
 
-	public int add(MemberVO memberVO)throws Exception{
-		//평문 password를 암호화
+	public int add(MemberVO memberVO) throws Exception {
+		// 평문 password를 암호화
 		memberVO.setPassword(passwordEncoder.encode(memberVO.getPassword()));
 		int result = memberDAO.add(memberVO);
-		
-		//회원의 Role 정보 저장
-		result = memberDAO.addMemberRole(memberVO);		
+
+		// 회원의 Role 정보 저장
+		result = memberDAO.addMemberRole(memberVO);
 		return result;
 	}
-	
-	//add 검증 메서드
-	//비번일치, id 중복 여부
-	public boolean checkMember(MemberVO memberVO, BindingResult bindingResult)throws Exception{
-		boolean check=false;
-		//check 가 true 라면 에러가 있다..
-		//check 가 false 라면 에러가 없다..
-		
-		
-		//annotation 검증 결과
-		check=bindingResult.hasErrors();
-		
-		//비번 검증
-		if(!memberVO.getPassword().equals(memberVO.getPasswordCheck())) {
-			check=true;
+
+	// add 검증 메서드
+	// 비번일치, id 중복 여부
+	public boolean checkMember(MemberVO memberVO, BindingResult bindingResult) throws Exception {
+		boolean check = false;
+		// check 가 true 라면 에러가 있다..
+		// check 가 false 라면 에러가 없다..
+
+		// annotation 검증 결과
+		check = bindingResult.hasErrors();
+
+		// 비번 검증
+		if (!memberVO.getPassword().equals(memberVO.getPasswordCheck())) {
+			check = true;
 			bindingResult.rejectValue("passwordCheck", "memberVO.password.equals");
-			
+
 		}
-		
-		//id중복 
+
+		// id중복
 		MemberVO result = memberDAO.getDetail(memberVO);
-		
-		if(result != null) {
-			check=true;
+
+		if (result != null) {
+			check = true;
 			bindingResult.rejectValue("username", "memberVO.username.equals");
 		}
-		
+
 		return check;
 	}
 
